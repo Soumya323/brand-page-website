f129a94398a3b17a4abf521c46a051f0cd31c451
Ripan <soumyaripan@gmail.com>
2023-07-15 23:26:07 +0530

    full design

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import './App.css';
+import Hero from './components/Hero';
+import Navigation from './components/navigation';
 
 const App = ()=>  {
   return (<div> 
-    <nav>
-      <div className="logo">
-      <img src="/images/brand_logo.png" alt="logo"></img>
-      </div>
-
-      <ul>
-        <li href='#'>Menu</li>
-        <li href='#'>Location</li>
-        <li href='#'>About</li>
-        <li href='#'>Contact</li>
-      </ul>
-
-      <button>Login</button>
-    </nav>
-
+  <Navigation/>
+<Hero/>
   </div>);
 };
 
